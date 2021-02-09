78e74461d58d38153468f39ebaba269d725a0edb
EtrieusVet <72852015+EtrieusVet@users.noreply.github.com>
2021-02-10 03:33:39 +0800

    Test

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from pretty_help import Navigation, PrettyHelp
 
 ### Variables ###
 
-fire = "fire"
+
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
 client = commands.Bot(command_prefix='!', intents=intents,
                       help_command=PrettyHelp(color=discord.Color.dark_gray(), active_time=(float('inf'))))
-
+lol = "lol"
 
 ### Commands ###
 
@@ -35,7 +35,7 @@ async def unload_132(ctx, extension, amount=1):
 @client.event
 async def on_ready():
     print("{0.user} has awoken!".format(client))
-    await client.change_presence(activity=discord.Game('with Stan'))
+    await client.change_presence(activity=discord.Game('with Etrieus'))
 
 
 for filename in os.listdir('./cogs'):
@@ -43,4 +43,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('ODA1Mjk2MzY4NzMzMTI2NzE2.YBY0rw.oE9jOCNPQ4eOzXycL5Id3ZAE0sc')
+client.run('ODA1MDM1OTU0ODg3ODUyMDUy.YBVCKA.2vmW4s8nm_gthYjr7350Pcy_CVk')
