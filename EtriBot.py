6f1980ae1e74feb06553482242768f1ba924ac3e
EtrieusVet <72852015+EtrieusVet@users.noreply.github.com>
2021-02-14 19:29:30 +0800

    Update

--- EtriBot.py
+++ EtriBot.py
@@ -9,7 +9,7 @@ from pretty_help import Navigation, PrettyHelp
 
 ### Variables ###
 
-#TEST
+#TESTSADSDADSD
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
 client = commands.Bot(command_prefix='!', intents=intents,
                       help_command=PrettyHelp(color=discord.Color.dark_gray(), active_time=(float('inf'))))
