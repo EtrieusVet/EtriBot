16d2d3223ecf518632c46adc3a363b55e6a28a14
EtrieusVet <72852015+EtrieusVet@users.noreply.github.com>
2021-02-16 07:39:59 +0800

    Upd

--- cogs/scommands.py
+++ cogs/scommands.py
@@ -9,14 +9,14 @@ class SCommands(commands.Cog, description="Commands only used by specific roles.
     def __init__(self, client):
         self.client = client
 
-        @commands.Cog.listener()
-        async def on_ready():
-            print("SCommands is online!")
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("SCommands is online!")
 
-        @commands.command(aliases=["d"])
-        @commands.has_any_role('Ze Creator', 'Anti BS Department', 'Ze alt of ze owner,', 'Special Boiz')
-        async def clear(self, ctx, amount=5):
-            await ctx.channel.purge(limit=amount)
+    @commands.command(aliases=["d"])
+    @commands.has_any_role('Ze Creator', 'Anti BS Department', 'Ze alt of ze owner,', 'Special Boiz')
+    async def clear(self, ctx, amount=5):
+        await ctx.channel.purge(limit=amount)
 
 def setup(client):
     client.add_cog(SCommands(client))
