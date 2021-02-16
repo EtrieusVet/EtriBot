33ffb1fadcbf16db086c2d0f7ec683492f23f03f
EtrieusVet <72852015+EtrieusVet@users.noreply.github.com>
2021-02-16 08:26:39 +0800

    Updateee

--- cogs/scommands.py
+++ cogs/scommands.py
@@ -18,6 +18,22 @@ class SCommands(commands.Cog, description="Commands only used by specific roles.
     async def Clear(self, ctx, amount=5):
         await ctx.channel.purge(limit=amount)
 
+    @commands.command(aliases=["mute"], brief="Mutes the specified moron.")
+    async def Mute(self, ctx, member: discord.Member, *, reason=None):
+        guild = ctx.guild
+        mutedRole = discord.utils.get(guild.roles, name = 'Muted')
+
+        if not mutedRole:
+            mutedRole = await guild.create_role('Muted')
+
+            for channel in guild.channels:
+                await channel.set_permissions(mutedRole, speak=False, send_messages=False)
+
+        await member.add_roles(mutedRole, reason=reason)
+        await ctx.send(f'Muted {member.mention} for reason {reason}')
+        await member.send(f'You are muted in the server {guild.name} for {reason}')
+
+
 def setup(client):
     client.add_cog(SCommands(client))
 
