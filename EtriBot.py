8c918e272406fec5554db37ad5388f3d3ddfd2bb
EtrieusVet <72852015+EtrieusVet@users.noreply.github.com>
2021-04-01 19:51:06 +0800

    Updateee

--- EtriBot.py
+++ EtriBot.py
@@ -39,4 +39,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('x8EoGWwbbAphpBE2ZUBW7H24--MBPP52')
+client.run('ODA1MDM1OTU0ODg3ODUyMDUy.YBVCKA.SAj0cYp4hMoTYWll8LTUhGay2H4')
